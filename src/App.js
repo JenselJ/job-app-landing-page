5aa629f7a7c12c62bacdf58c719ea53ee1a50dcf
JenselJ <saneer2002@gmail.com>
2022-08-28 11:59:22 +1000

    20:30

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     
+      <div className='bg-slate-700 text-white px-8 py-16'>
+        <div className="max-w-xl grid grid-cols-1 gap-8">
+          <h2 className="text-xl uppercase font-bold">Become a host</h2>
+          <h1 className="text-6xl font-bold">Hose your space, share your world</h1>
+          <p className="text-lg">
+            The art of hosting is rooted in thoughtful design. 
+            Share your unique aesthetic with guests and earn extra 
+            income on a schedule that works for you.
+          </p>
+          <button className='bg-red-500 py-2 px-6 text-lg rounded-md w-48'>Try Hosting</button>
+        </div>
+      </div>
+
+     
+      <h2>Your next chapter, made possible by Hosting</h2>
+
+      <h3>Share what you love</h3>
+      <p>
+        Indulge your love for design when you host Airbnb guests, who are drawn to authentic travel in creatively curated spaces.
+      </p>
+
+      <h3>
+        Host how you want to
+      </h3>
+      <p>
+        Airbnb gives you the tools and support to earn extra income, be your own boss and invest in your passions.
+      </p>
+
+    </div>
+  )
+}
+
+export default App
